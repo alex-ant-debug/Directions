59bbb8c54bafa520e75f26b059c6409f4ff7924d
Lemtugin <lemtugin@radmirtech.com.ua>
2020-02-21 22:46:39 +0200

    add function detectIsTable

--- parser.cpp
+++ parser.cpp
@@ -12,22 +12,75 @@ public:
 Parser()
 {}
 
-Parser(string file)
+Parser(string file, char symbol = ' ')
 {
     this->fileName = file;
+    this->rowSeparator = symbol;
 }
 
 vector<string> splitRow(char *rowPtr)
 {
     vector<string> rowData;
-    char *pch = strtok(rowPtr, " ");
+    char *pch = strtok(rowPtr, &rowSeparator);//" "
 
     while (pch != NULL)                         // пока есть лексемы
       {
           rowData.push_back(pch);
-          pch = strtok(NULL, " ");
+          pch = strtok(NULL, &rowSeparator);//" "
       }
     return rowData;
+}
+
+unsigned int countNumberOfDelimiters(string lineTable, char separator)
+{//найти стандартную функцию подсчета разделителей в строке
+    unsigned int quantity = 0;
+    size_t found = 0;
+
+    found = lineTable.find_first_of(separator,found);
+
+    while(found != string::npos)
+    {
+        quantity++;
+        found = lineTable.find_first_of(separator,found+1);
+    }
+
+    return quantity;
+}
+
+//automaticSeparatorDetection(void)
+//int separators[3] = {',', '.', ' '};
+
+bool detectIsTable(char separator)
+{
+    ifstream fileTable(this->fileName);
+    string lineTable;
+    bool isTable = true;
+
+    unsigned int initialValue = 0;
+
+    if(!getline(fileTable, lineTable))
+    {
+        throw 98;
+    }
+
+    initialValue = countNumberOfDelimiters(lineTable, separator);
+
+    if(initialValue == 0)
+    {
+        return false;
+    }
+
+    while (getline(fileTable, lineTable))
+    {
+        int quantity = countNumberOfDelimiters(lineTable, separator);
+        if(quantity != initialValue)
+        {
+            isTable = false;
+            break;
+        }
+    }
+
+    return isTable;
 }
 
 
@@ -63,6 +116,7 @@ vector<vector<string>> getParsedData(void)
 
 protected:
 string fileName;
+char rowSeparator;
 vector<vector<string>> parsedData;
 
 };
